ea8d57e65411c0a6bb927e4b19c0e87176811068
Luca Di Vittorio <luca.div@hotmail.com>
2022-10-19 17:37:46 +0100

    selected root all the styles and specific variable

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,19 @@
 console.log('Hello from index.js!');
 // color = document.getElementById('color');
 // color.setAttribute(value, var(--))
+const root = document.querySelector(':root');
+// console.log(root);
+// function getCssVariables() {
+//   // Get the styles (properties and values) for the root
+//   const rs = getComputedStyle(r);
+//   // Alert the value of the --blue variable
+//   alert("The value of --blue is: " + rs.getPropertyValue('--blue'));
+// }
+const allTheStyles = getComputedStyle(root);
+const spacing = allTheStyles.getPropertyValue('--spacing');
+// console.log(spacing);
+// function myFunction_set() {
+//   // Set the value of variable --blue to another value (in this case "lightblue")
+//   r.style.setProperty('--blue', 'lightblue');
+// }
+// console.log(spacing.setProperty);
