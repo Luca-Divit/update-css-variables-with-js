d303297e212e9ee7aa4e9e11433a4781cc96e6aa
Luca Di Vittorio <luca.div@hotmail.com>
2022-10-20 15:42:09 +0100

    manipulated successfully css variables live on the dom using javascript

--- javascript/index.js
+++ javascript/index.js
@@ -10,8 +10,37 @@ const root = document.querySelector(':root');
 //   alert("The value of --blue is: " + rs.getPropertyValue('--blue'));
 // }
 const allTheStyles = getComputedStyle(root);
-const spacing = allTheStyles.getPropertyValue('--spacing');
-// console.log(spacing);
+// const spacing = allTheStyles.getPropertyValue('--spacing');
+const spacingBar = document.getElementById('spacing');
+const blurBar = document.getElementById('blur')
+const colorSelector = document.getElementById('color')
+// console.log(spacingBar.value);
+
+// const increaseSpacing = () => {
+//   spacing = `${spacingBar.value / 250 * 4}em`;
+// }
+
+spacingBar.addEventListener('input', (event) => {
+  // console.log(event.target.valueAsNumber);
+  let value = event.target.valueAsNumber;
+  root.style.setProperty('--spacing', `${value / 250 * 4}em`);
+  // spacing.style.setPr = `${value / 250 * 4}em`;
+})
+
+blurBar.addEventListener('input', (event) => {
+  // console.log(event.target.valueAsNumber);
+  let value = event.target.valueAsNumber;
+  root.style.setProperty('--blur', `blur(${value / 250}em)`);
+  // spacing.style.setPr = `${value / 250 * 4}em`;
+})
+
+colorSelector.addEventListener('input', (event) => {
+  // console.log(event.target.value);
+  let value = event.target.value;
+  root.style.setProperty('--color', `${value}`);
+  // spacing.style.setPr = `${value / 250 * 4}em`;
+})
+
 // function myFunction_set() {
 //   // Set the value of variable --blue to another value (in this case "lightblue")
 //   r.style.setProperty('--blue', 'lightblue');
